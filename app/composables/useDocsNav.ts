e58cf646ac0ca812588948affad3c28e4ce5f4f5
hupeh <hupeh@qq.com>
2026-03-15 09:12:01 +0800

    fix: resolve docs nav without index page

--- app/composables/useDocsNav.ts
+++ app/composables/useDocsNav.ts
@@ -18,9 +18,8 @@ export function useDocsNav() {
 
       // Check if group index is not exists and default to first child
       const originalPath = item.path;
-      if (item.children?.length && !item.children.some((c) => c.path === originalPath)) {
-        item.path = item.children[0].path;
-      }
+
+      item.path = resolveItemPath(item);
 
       return {
         ...item,
@@ -34,7 +33,8 @@ export function useDocsNav() {
   });
 
   const activeSection = computed(() =>
-    links.value.find((l) => route.path.startsWith(l.originalPath)),
+    // links.value.find((l) => route.path.startsWith(l.originalPath)),
+    resolveActiveSection(links.value, route.path),
   );
   const activeLinks = computed(() => (activeSection.value?.children || []).filter(Boolean));
 
@@ -43,4 +43,60 @@ export function useDocsNav() {
     activeSection,
     activeLinks,
   });
+}
+
+function resolveItemPath(item: ContentNavigationItem): string | undefined {
+  // Check if group index is not exists and default to first child
+  if (item.children?.length && !item.children.some((c) => c.path === item.path)) {
+    return resolveItemPath(item.children[0]);
+  }
+  return item.path;
+}
+
+function resolveActiveSection(links: ContentNavigationItem[], path: string) {
+  const active = links.find((l) => isPathPrefix(path, l.originalPath));
+  if (active) {
+    return active;
+  }
+
+  const parts = path.split("/").filter(Boolean);
+  while (parts.length) {
+    const part = "/" + parts[0];
+    if (links.some((l) => l.originalPath.startsWith(part))) {
+      break;
+    }
+    parts.shift();
+  }
+
+  while (parts.length) {
+    const part = "/" + parts[parts.length - 1];
+    if (links.some((l) => isPartial(l.originalPath, part))) {
+      break;
+    }
+    parts.pop();
+  }
+
+  path = parts.join("/");
+  if (!path) {
+    return null;
+  }
+  path = "/" + path;
+
+  return links.find((l) => isPartial(l.originalPath, path));
+}
+
+function isPartial(originalPath: string, path: string) {
+  const start = originalPath.indexOf(path);
+  if (start === -1) {
+    return false;
+  }
+  const end = start + path.length;
+  return (
+    (start === 0 || originalPath[start - 1] === "/") &&
+    (end === originalPath.length || originalPath[end] === "/")
+  );
+}
+
+function isPathPrefix(path: string, prefix: string) {
+  return path.startsWith(prefix) && (path.length === prefix.length || path[prefix.length] === "/");
 }
